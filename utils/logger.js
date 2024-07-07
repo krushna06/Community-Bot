dfd0e8cb049c59bad28663fbc862a0d936103fea
n0step_ <krushnavyas06@gmail.com>
2024-07-07 15:59:18 +0000

    Made the logger better

--- utils/logger.js
+++ utils/logger.js
@@ -1,11 +1,14 @@
 const { createLogger, format, transports } = require('winston');
+const chalk = require('chalk');
+const moment = require('moment-timezone');
 
 const logger = createLogger({
     level: 'info',
     format: format.combine(
         format.timestamp(),
         format.printf(({ timestamp, level, message }) => {
-            return `${timestamp} | bot_0 | ${level.toUpperCase().padEnd(8)} | ${message}`;
+            const istTimestamp = moment(timestamp).tz('Asia/Kolkata').format('YYYY-MM-DD HH:mm:ss');
+            return `${chalk.green(istTimestamp)} | bot_0 | ${chalk.cyan(level.toUpperCase().padEnd(8))} | ${chalk.white(message)}`;
         })
     ),
     transports: [
